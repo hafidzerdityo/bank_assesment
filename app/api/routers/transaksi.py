87cb465dcc6887e57405a8530311bc1aa8108c31
hafidz erdityo <hafidze15@gmail.com>
2023-06-30 11:19:30 +0700

    fix generate number

--- app/api/routers/transaksi.py
+++ app/api/routers/transaksi.py
@@ -21,7 +21,7 @@ async def create_tabung(create_tabung_payload: transaksi_schema.RequestTabung):
         )
 
 @router.post('/tarik', tags=["Transaksi"], status_code=status.HTTP_200_OK)
-async def create_tarik(create_tarik_payload: transaksi_schema.RequestTabung):
+async def create_tarik(create_tarik_payload: transaksi_schema.RequestTarik):
     try:
         data_creation_success = await transaksi_repo.create_tarik(create_tarik_payload)
         return data_creation_success 
--- app/core/repositories/utils.py
+++ app/core/repositories/utils.py
@@ -3,6 +3,7 @@ import hashlib
 import random
 import core.models.db_model as db_model
 from db.database_config import database
+import time 
 
 
 def get_current_datetime():
@@ -12,11 +13,15 @@ def hash_with_sha_256(text):
     return hashlib.sha256(text.encode()).hexdigest()
 
 async def generate_unique_account_number() -> str:
-    digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    
+    digits = list(range(0,10))
+    digits = [str(i) for i in digits]
+    retry = 100
     while True:
         account_number = ''.join(random.choices(digits, k=10))
         check_no_rek = db_model.Nasabah.select().where(db_model.Nasabah.c.no_rekening == account_number)
         check_no_rek = await database.fetch_one(check_no_rek)
         if not check_no_rek:
             return account_number
+        retry += 1
+        if retry > 100:
+            raise Exception("Timeout")
